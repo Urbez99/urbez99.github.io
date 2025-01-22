2fe27331c092f07522b3286dcf4a81868c7558b7
Urbez99 <162153198+Urbez99@users.noreply.github.com>
2025-01-22 18:58:24 +0100

    Actualizacion astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,9 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://urbez99.github.io"
+  site: "https://urbez99.github.io",
+    devToolbar : {
+  enabled: false
+},
   integrations: [tailwind()]
 });
